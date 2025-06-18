281e5ed5923a0a4b907d5d02d8e99ce072adcdc4
makochannnnnn <25.m.yamamoto.nutfes@gmail.com>
2025-06-18 17:21:29 +0900

    initial-commit-hobby

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
       <Link href="/technology" className="text-blue-500 hover:underline mt-4">技術</Link>
       <Link href="/contact" className="text-blue-500 hover:underline mt-4">お問い合わせ</Link>
       <Link href="/interests" className="text-blue-500 hover:underline mt-4">興味</Link>
+      <Link href="/hobby" className="text-blue-500 hover:underline mt-4">趣味</Link>
       <button className="bg-white text-black p-2 rounded">Click me</button>
     </main>
   );
