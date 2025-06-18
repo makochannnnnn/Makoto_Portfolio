1bf37101a0bd5dd2eeeb902ff8e2f0f73d5482ae
makochannnnnn <25.m.yamamoto.nutfes@gmail.com>
2025-06-18 16:54:58 +0900

    [feat]base-components

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold mb-8">Welcome</h1>
+      <Image
+        src="/my_face.jpg"
+        alt="Next.js Logo"
+        width={200}
+        height={200}
+        className="rounded-full mb-8"/>
       <Name name="Makoto"/>
       <p className="mt-8 text-gray-600">This is my first Next.js page with App Router and Tailwind CSS.</p>
       <button className="bg-white text-black p-2 rounded">Click me</button>
